3143d9798208e43daeae7978364ca6b248cf0a29
jugol-kumar <jk23717933@gmail.com>
2023-09-17 03:15:41 +0600

    fix chat time

--- resources/views/recruiters/chatting/index.blade.php
+++ resources/views/recruiters/chatting/index.blade.php
@@ -106,11 +106,11 @@
                                             <div class="chat-message-left pb-4">
                                             <div>
                                                 <img src="{{ $chat?->sender?->photo }}" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
-                                                <div class="text-muted small text-nowrap mt-2">2:34 am</div>
                                             </div>
                                             <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
                                                 <div class="font-weight-bold mb-1">{{ $chat?->sender?->name }}</div>
-                                                {{ $chat?->body }}
+                                                <p>{{ $chat?->body }}</p>
+                                                <small class="text-muted small text-nowrap mt-2">2:34 am</small>
                                             </div>
                                         </div>
                                             @endif
